18001bd4535cfa2f19c67dc8dae3ce725fe18183
Maria Bondarenko <imya_maria@mail.ru>
2024-02-22 17:00:45 +0100

    changed js file

--- scripts/script.js
+++ scripts/script.js
@@ -121,6 +121,7 @@ window.onload = function () {
 }
 
 console.log("The end");
+console.log("The end 2");
 
 
 //Создайте страницу для регистрации на сайте по Figma-макету и подключите к ней javascript и css файлы.
